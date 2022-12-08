daabc282fd59485847eb4fb1a35295cf140ace75
Vaibhavi Santosh Kale <112152930+Vaibhavi-Santosh-Kale@users.noreply.github.com>
2022-12-08 12:47:15 +0530

    Add files via upload

---
+++ javascript_Object_and_JSON.js
@@ -0,0 +1,55 @@
+console.log("Objects.......");
+
+// const student = {
+//   name: "ruturaj",
+//   age: 17,
+//   marks: {
+//     maths: 75,
+//     Science: 70,
+//   },
+//   greet: function () {
+//     console.log("Hello");
+//   },
+// };
+// console.log(student.age);
+// console.log(student.marks.Science);
+// student.greet();
+
+// let number = "23.32";
+// let result = parseInt(number);
+// console.log(result);
+
+// let number1 = "23.32";
+// let result1 = parseFloat(number1);
+// console.log(result1);
+
+// let string = "Vaibhavi";
+// let result2 = parseInt(string);
+// console.log(result2);
+
+// const person = {};
+// person.name = "sara";
+// person.greet = function () {
+//   console.log("Good Morning");
+// };
+// person.age = 20;
+
+// console.log(person);
+
+// const flowers = {
+//   name: "Rose",
+//   colour: "Red",
+//   greet: function () {
+//     console.log(
+//       "My most Fevorite flower is " + "" + this.colour + " " + this.name
+//     );
+//   },
+// };
+// flowers.greet();
+
+// const letters = new Set(["a", "b", "c", "a", "b"]);
+// let text = "";
+// letters.forEach(function (value) {
+//   text += value;
+//   console.log(value);
+// });
